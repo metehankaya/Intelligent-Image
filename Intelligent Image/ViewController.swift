ecae70115be7543b627ef4962f902b72578542e6
Metehan Kaya <metehankaya25@gmail.com>
2018-12-06 17:03:37 +0300

    Initial Commit

---
+++ Intelligent Image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intelligent Image
+//
+//  Created by Metehan Kaya on 6.12.2018.
+//  Copyright © 2018 Metehan Kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
